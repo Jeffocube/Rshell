6fd4fc6268b6babe5c19a2e1651890b1d1ecca6b
Jeffery Lai <jlai030@hammer.cs.ucr.edu>
2019-02-11 17:26:56 -0800

    Unfinished command

--- header/command.h
+++ header/command.h
@@ -5,11 +5,28 @@
 #include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <input.h>
+#include "input.h"
 using namespace std;
 
-class command : public input{
+class command : public Input{
     private:
-    enum connector; 
+        enum connector; 
+        string activity;
+        vector<string> args;
+        vector<Input*> comm;
+    public:
+        command(enum conn, string act, vector<string> args, vector<Input*> comm){
+            connector = conn;
+            activity = act;
+            this->args = args;
+            this->comm = comm;
+        }
+        void execute(){
+            this->executeHelper();
+            unsigned numChildren = comm.size();
+            for(unsigned i = 0; i < numChildren; i++){
+                comm.at(i)->execute();
+            }
+        }
 };
 #endif
