c44bc0f634b30def669985975851429dd40404ad
Kevin <klee116@ucr.edu>
2019-02-15 02:24:35 -0800

    halfway through parse

--- header/parse.h
+++ header/parse.h
@@ -6,10 +6,51 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "input.h"
+#include <sstream>
+#include <cstring>
+#include "command.h"
+using namespace std;
 
-class parse{
-    public:
-    command* 
+void parse(string userInput, vector<Input*> outVector)
+{
+	int marker = 0;
+	vector<string> commandStorage; //putting everything in her
+	for (int i = 0; i < userInput.size(); i++)
+	{
+		if (userInput.at(i) == ';') // || userInput.at(i) == '|') 
+		{
+			string temp;
+			temp = userInput.substr(marker, i - marker + 1);
+			commandStorage.push_back(temp); temp.clear();
+
+			marker = i+2; //assume next char is ' '
+			i+=2;
+			//notdone
+		}//break into
+		else if (userInput.at(i) == '&' || userInput.at(i) == '|')
+		{
+			if (i != userInput.size() - 1)
+			{
+				if (userInput.at(i+1) == userInput.at(i))
+				{
+					string temp;
+					temp = userInput.substr(marker, i - marker + 2);
+					commandStorage.push_back(temp); temp.clear();
+
+					marker = i+3; i+=3; //extra 1 because two char connector
+				}
+			}
+					
+		}
+	}
+
+
+return;
+
+
+	//create commandLeaf objects to place in outVector
+	
+	
 }
 
 #endif
